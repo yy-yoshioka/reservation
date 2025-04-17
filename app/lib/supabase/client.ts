42d63f27a9c1fc55e50636f83db237042f7edaee
yy-yoshioka <14ss098@gmail.com>
2025-04-17 13:43:14 +0900

    fix create client

--- app/lib/supabase/client.ts
+++ app/lib/supabase/client.ts
@@ -1,8 +1,6 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { createClient } from "@supabase/supabase-js";
 
-export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
-}
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
